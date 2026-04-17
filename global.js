28843dd5575d72c5804d7691d2ed17e4fcff801c
van013-svg <van013@ucsd.edu>
2026-04-17 16:34:54 -0700

    Adding the navigation menu

--- global.js
+++ global.js
@@ -4,11 +4,16 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/portfolio/";         // GitHub Pages repo name
+
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
-  { url: 'resume/', title: 'Resume' }
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/van013-svg', title: 'GitHub' }
 ];
 
 let nav = document.createElement('nav');
@@ -17,5 +22,8 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
