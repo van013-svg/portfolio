b0a41248efe8f3e5d8856793001cfd7302da5394
van013-svg <van013@ucsd.edu>
2026-04-17 14:11:08 -0700

    adding new nav menu

--- global.js
+++ global.js
@@ -4,10 +4,18 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' }
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-currentLink?.classList.add('current');
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
