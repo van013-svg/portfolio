ca5fb31dbb86adf5722d7db7754b3d64c4bbf884
van013-svg <van013@ucsd.edu>
2026-04-17 17:29:46 -0700

    Update global.js

--- global.js
+++ global.js
@@ -64,8 +64,18 @@ let select = document.querySelector('select');
 select.addEventListener('input', function (event) {
   console.log('color scheme changed to', event.target.value);
 
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+
+  localStorage.colorScheme = event.target.value;
+  
+  if ('colorScheme' in localStorage) {
+  let scheme = localStorage.colorScheme;
+
   document.documentElement.style.setProperty(
     'color-scheme',
-    event.target.value
+    scheme === 'automatic' ? 'light dark' : scheme
   );
+
+  select.value = scheme;
+}
 });
