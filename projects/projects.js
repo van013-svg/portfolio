6f55ade5325031ab8e219328787161b887ec9a79
van013-svg <van013@ucsd.edu>
2026-04-24 10:43:11 -0700

    Adding count to projects page

--- projects/projects.js
+++ projects/projects.js
@@ -4,4 +4,10 @@ const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
 
+const titleEl = document.querySelector('.projects-title');
+
+if (titleEl) {
+  titleEl.textContent = projects.length;
+}
+
 renderProjects(projects, projectsContainer, 'h2');
