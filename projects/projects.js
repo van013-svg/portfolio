e4dfd1dcd18d1735b9fc0ac11d7716abca6eff79
van013-svg <van013@ucsd.edu>
2026-05-01 10:57:55 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -22,3 +22,29 @@ if (titleEl) {
 }
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let data = [1,2];
+
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', 'red');
+});
