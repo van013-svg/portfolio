022fb734a26c0bd50b239a4fee9c334b662711b3
van013-svg <van013@ucsd.edu>
2026-05-01 11:26:08 -0700

    Adding a legend

--- projects/projects.js
+++ projects/projects.js
@@ -16,9 +16,16 @@ if (titleEl) {
 
 renderProjects(projects, projectsContainer, 'h2');
 
-let data = [1,2,3,4,5,5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 
 let arcData = sliceGenerator(data);
 
@@ -31,4 +38,12 @@ arcs.forEach((arc, idx) => {
     .append('path')
     .attr('d', arc)
     .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
