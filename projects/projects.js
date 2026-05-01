ab552d9359e7d05d3f1337d14725433fd8bac413
van013-svg <van013@ucsd.edu>
2026-05-01 11:12:54 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,11 +2,6 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI
-});
-
 d3.select('#projects-pie-plot').append('path').attr('d', arc).attr('fill', 'red');
 
 import { fetchJSON, renderProjects } from '../global.js';
