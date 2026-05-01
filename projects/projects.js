2fbeea374a158e8d674b461716aa387b09d601d3
van013-svg <van013@ucsd.edu>
2026-05-01 11:43:16 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -16,6 +14,8 @@ if (titleEl) {
 
 renderProjects(projects, projectsContainer, 'h2');
 
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
 let data = [
   { value: 1, label: 'apples' },
   { value: 2, label: 'oranges' },
@@ -29,13 +29,19 @@ let sliceGenerator = d3.pie().value((d) => d.value);
 
 let arcData = sliceGenerator(data);
 
-let arcs = arcData.map((d) => arcGenerator(d));
-
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+d3.select('#projects-pie-plot')
+  .selectAll('path')
+  .data(arcData)
+  .enter()
+  .append('path')
+  .attr('d', arcGenerator)
+  .attr('fill', (d, i) => colors(i));
 
 let legend = d3.select('.legend');
 
-data.forEach((d, idx) => {
+arcData.forEach((d, idx) => {
   let li = legend.append('li')
     .attr('class', 'legend-item')
     .attr('style', `--color:${colors(idx)}`);
@@ -44,5 +50,5 @@ data.forEach((d, idx) => {
     .attr('class', 'swatch');
 
   li.append('span')
-    .text(d);
+    .text(d.data.label);
 });
