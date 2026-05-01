3d428f03e6f45a9b381fe793ea6e67fab1507d11
van013-svg <van013@ucsd.edu>
2026-05-01 10:43:28 -0700

    Drawing our circle path with D3

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,14 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
