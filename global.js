f19f5c1d3551b28430ae0c3cb76d37e34a31f978
van013-svg <van013@ucsd.edu>
2026-05-01 00:16:13 -0700

    display year

--- global.js
+++ global.js
@@ -125,11 +125,17 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <h3>${project.title}</h3>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
+      
+      <div class="project-text">
+        <p class="project-description">
+          ${project.description}
+        </p>
 
-      <p class="project-year">
+        <p class="project-year">
           ${project.year}
         </p>
+      </div>
+      
     `;
 
     containerElement.appendChild(article);
