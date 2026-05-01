4d88f9b698b2149b36f0b09cc65962c5be013b67
van013-svg <van013@ucsd.edu>
2026-05-01 13:01:59 -0700

    changing legend to be project years

--- projects/projects.js
+++ projects/projects.js
@@ -14,20 +14,19 @@ if (titleEl) {
 
 renderProjects(projects, projectsContainer, 'h2');
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let sliceGenerator = d3.pie().value((d) => d.value);
-
 let arcData = sliceGenerator(data);
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
