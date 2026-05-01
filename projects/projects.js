184a917a3f0b5d006345e2c411d1980f27cc966b
van013-svg <van013@ucsd.edu>
2026-05-01 13:12:11 -0700

    filtering projects based on search

--- projects/projects.js
+++ projects/projects.js
@@ -51,3 +51,17 @@ arcData.forEach((d, idx) => {
   li.append('span')
     .text(`${d.data.label} (${d.data.value})`);
 });
+
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('change', (event) => {
+  // update query value
+  query = event.target.value;
+  // filter projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  // render filtered projects
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+});
