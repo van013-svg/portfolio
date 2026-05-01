caea6dfb4aa00e48dc17e230a3eee5510ab112de
van013-svg <van013@ucsd.edu>
2026-05-01 11:15:40 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,8 +2,6 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-d3.select('#projects-pie-plot').append('path').attr('d', arc).attr('fill', 'red');
-
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -37,7 +35,7 @@ for (let d of data) {
 
 let arcs = arcData.map((d) => arcGenerator(d));
 
-let colors = ['gold', 'purple'];
+let colors = ['purple', 'gold'];
 
 arcs.forEach((arc, idx) => {
   d3.select('#projects-pie-plot')
